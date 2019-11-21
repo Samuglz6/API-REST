3e48ed325169b19bff0a275c1db5e0e9c738c8b7
Samuglz6 <samu_sgl_96@hotmail.com>
2019-11-22 00:33:29 +0100

    recurso Talleres
    Co-Authored-By: null <BekaBekeri@users.noreply.github.com>

---
+++ recursos/Coches.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Blueprint, jsonify, abort, make_response, request
+
+class Coche:
+	def __init__(self, idCoche, matricula, marca, fechaFabricacion, tipoAveria):
+		self.idCoche = idCoche
+		self.matricula = matricula
+		self.marca = marca
+		self.fechaFabricacion = fechaFabricacion
+        self.tipoAveria = tipoAveria
+
+####################################################################
+# Example:
+listaCoches = [ Coche(1, '1234ABC', 'Volvo', '2010', 1),
+                Coche(2, '8473LSM', 'Toyota', '2012', 4),
+                Coche(3, '7973KDD', 'Seat', '2015', 3),
+	     	    Coche(4, '4441NFC', 'Ranault', '2018', 2)]
+
+####################################################################
+
+coches_api = Blueprint('coches_api', __name__)
+
+#Devolver todos los coches existentes
+@coches_api.route('/recursos/coches/', methods=['GET'])
+def obtenerCoches():
+	return jsonify({'coches': listaCoches})
+
+#Devolver los datos de un coche segun el id indicado
+@coches_api.route('/recursos/coches/<int:idCoche>/', methods=['GET'])
+def obtenerUnCoche(idCoche):
+	for coche in listaCoches:
+		if coche.idCoche == idCoche:
+			return jsonify({'coche': coche})
+    abort(404)
+
+#Añade un nuevo coche
+@coches_api.route('/recursos/coches/', methods=['POST'])
+def crearCoche():
+	if not request.json or not 'idCoche' in request.json:
+        	abort(404)
+    idCoche = request.json.get('idCoche')
+	matricula = request.json.get('matricula')
+    marca = request.json.get('marca')
+	fechaFabricacion = request.json.get('fechaFabricacion')
+    tipoAveria = request.json.get('tipoAveria')
+
+	coche = Coche(regId, matricula, marca, fechaFabricacion, tipoAveria)
+    listaCoches.append(coche)
+
+    return jsonify({'car':car}),201
+
+#Elimina un coche existente
+@talleres_api.route('/recursos/talleres/<int:id>/', methods=['DELETE'])
+def borrarCoche(id):
+	coche = [coche for coche in listaCoches if coche['idCoche'] == id]
+	listaCoches.remove(coche[0])
+	return jsonify({}), 204 # No content
+
+# Manejo errores 404
+@devices_api.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'ERROR': 'No se ha encontrado el recurso'}),404)
---
+++ recursos/Talleres.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Blueprint, jsonify, abort, make_response, request
+
+class Taller:
+
+	def __init__(self, idTaller, nombreTaller, direccion, abierto, marca):
+		self.idTaller = idTaller
+		self.nombreTaller = nombreTaller
+        self.direccion = direccion
+        self.abierto = abierto
+        self.marca = marca
+
+####################################################################
+# Example:
+listaTalleres = [ Taller(1, 'Talleres Manolo','Calle ejemplo 13', True, 'Seat'),
+             	  Taller(2, 'Bekario','Calle inventada 31', False, 'Volvo'),
+             	  Taller(3, 'Reparaciones Lopez','Calle Falsa 2', True, 'Toyota'),
+	     	 	  Taller(4, 'Arreglos Linde', 'Calle ficticia 12', True, 'Renault')]
+
+####################################################################
+
+talleres_api = Blueprint('talleres_api', __name__)
+
+#Mostrar todos los talleres
+@talleres_api.route('/recursos/talleres/', methods=['GET'])
+def obtenerTalleres():
+	return jsonify({'talleres': talleres})
+
+#Devolver los datos de un taller segun el id indicado
+@talleres_api.route('/recursos/talleres/<int:idTaller>/', methods=['GET'])
+def obtenerTaller(nombreTaller):
+        for taller in talleres:
+                if taller.get('idTaller') == idTaller:
+                        return jsonify({'taller':taller})
+        abort(404)
+
+#Añade un nuevo taller
+@talleres_api.route('/recursos/talleres/', methods=['POST'])
+def crearTaller():
+        if not request.json or not 'nombreTaller' in request.json:
+                abort(404)
+		id = talleres[-1].get('id')+1
+		nombreTaller = request.json.get('nombreTaller')
+		direccion = request.json.get('direccion')
+		marca = request.json.get('marca')
+        abierto = False
+        taller = {'id': id, 'nombreTaller': nombreTaller, 'marca': marca,'abierto': abierto}
+        talleres.append(taller)
+        return jsonify({'taller':taller}),201
+
+#Elimina un taller existente
+@talleres_api.route('/recursos/talleres/<int:id>/', methods=['DELETE'])
+def borrarTaller(id):
+	taller = [taller for taller in talleres if taller['idTaller'] == id]
+	listaTalleres.remove(taller[0])
+	return jsonify({}), 204 # No content
+
+
+#Definimos la respuesta para el codigo de error 404
+@talleres_api.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'Error': 'No se ha encontrado el recurso'}),404)
